7a32c940cd6632a5c4b361430998c517acf8a28a
Meltem-Karaagac <67814400+Meltem-Karaagac@users.noreply.github.com>
2021-01-19 10:42:28 -0500

    Update urls.py

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from .views import PostList, PostDetail, CommentList
 
-urlspatterns = [
-    path('',)
+app_name = 'blog'
 
+urlpatterns = [
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listcreate'),
+    path('comment/', CommentList.as_view(), name='comment-create'),
 ]
